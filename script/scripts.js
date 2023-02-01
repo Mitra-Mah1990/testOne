5be0d0dd39bcff139547ccab5bc4b6bd022811aa
Mitra-Mah1990 <123596283+Mitra-Mah1990@users.noreply.github.com>
2023-02-01 15:54:14 -0500

    Lab 1

---
+++ script/scripts.js
@@ -0,0 +1,27 @@
+let fName = "Mitra";
+
+let age = 32;
+
+let isStudent = true;
+
+console.log(`My name is  ${fName}` + ` and I am  ${age} years odl` + ` and  ${isStudent} I am a student  `);
+
+let newAgae = age + 1;
+
+console.log(newAgae);
+
+let temperature = 33.0;
+
+if (temperature <= 32){
+    console.log("The temprature is below freezing");
+}else{
+    console.log("The temprature is above freezing");
+};
+
+let login_attempts = 3;
+
+if(login_attempts >= 4){
+    console.log("Too many login attempts")
+}else{
+    console.log("login success");
+}
